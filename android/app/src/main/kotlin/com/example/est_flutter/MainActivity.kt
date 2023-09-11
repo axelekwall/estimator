dc0ab9ae359629a9e63cc82596fb4d4d38d210a7
Axel Ekwall <git@axelekwall.se>
2023-09-11 15:08:30 +0200

    reorganize and rename files

---
+++ android/app/src/main/kotlin/com/example/est_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package se.axelekwall.estimator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- est_flutter/android/app/src/main/kotlin/com/example/est_flutter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.est_flutter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
